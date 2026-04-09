caf85d2e709ef2c824bfb7e48802ce5984333526
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-09 17:23:47 +0800

    Delete assets directory

--- assets/useMessage-gTlo8vYO.js
+++
@@ -1,1 +0,0 @@
-import{k as t,i as r,l as o}from"./index-Bftt7xpx.js";const u=t("msg",()=>{const s=r();return{msg:o({get:()=>s.value,set:e=>{s.value=e}}),_msg:s}},{persist:!0});export{u};
--- assets/useNotify-lRF60x1a.js
+++
@@ -1,1 +0,0 @@
-import{k as o,i as s,l as i}from"./index-Bftt7xpx.js";const f=o("notify",()=>{const t=s();return{notify:i({get:()=>t.value,set:e=>{t.value=e}}),_notify:t}},{persist:!0});export{f as u};
