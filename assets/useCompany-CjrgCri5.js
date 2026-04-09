244e5c5dd004d835aad49500a34a01fd6662d0aa
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-09 17:25:41 +0800

    Add files via upload

---
+++ assets/useCompany-CjrgCri5.js
@@ -0,0 +1,1 @@
+import{k as e,i as s,l as o}from"./index-CMZYFy8t.js";const u=e("company",()=>{const t=s();return{data:o({get:()=>t.value,set:a=>{t.value=a}}),_data:t}},{persist:!0});export{u};
---
+++ assets/useMessage-DgsPHvw6.js
@@ -0,0 +1,1 @@
+import{k as t,i as r,l as o}from"./index-CMZYFy8t.js";const u=t("msg",()=>{const s=r();return{msg:o({get:()=>s.value,set:e=>{s.value=e}}),_msg:s}},{persist:!0});export{u};
