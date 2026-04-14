75cca9dae8ee8b037fadb0a280bfcfc14cd52423
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-14 23:33:02 +0800

    Add files via upload

---
+++ assets/useNotify-as9diHvv.js
@@ -0,0 +1,1 @@
+import{W as o,r as s,X as r}from"./index-BnJknMvC.js";const i=o("notify",()=>{const t=s();return{notify:r({get:()=>t.value,set:e=>{t.value=e}}),_notify:t}},{persist:!0});export{i as u};
