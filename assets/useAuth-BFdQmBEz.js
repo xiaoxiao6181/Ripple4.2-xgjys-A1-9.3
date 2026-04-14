a78df3df4881cb7c5f258fd0a0347a55978e963a
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-14 23:28:10 +0800

    Delete assets directory

--- assets/useAuth-BFdQmBEz.js
+++
@@ -1,1 +0,0 @@
-import{W as u,r as s,X as a}from"./index-DZDjMtgF.js";const h=u("auth",()=>{const t=s();return{auth:a({get:()=>t.value,set:e=>{t.value=e}}),_auth:t}},{persist:!0});export{h as u};
--- assets/useYuEBao-Lu6Ogdx3.js
+++
@@ -1,1 +0,0 @@
-import{W as e,r as o,X as s}from"./index-DZDjMtgF.js";const d=e("YuEBao",()=>{const t=o(void 0);return{data:s({get:()=>t.value,set:a=>{t.value=a}}),_data:t}},{persist:!0});export{d as u};
