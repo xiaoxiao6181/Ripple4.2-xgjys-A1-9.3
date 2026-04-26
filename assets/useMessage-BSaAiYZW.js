96786e0e1ff11dcc67c032a492d716ab3d32b81f
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-26 11:42:25 +0800

    Delete assets directory

--- assets/useMessage-BSaAiYZW.js
+++
@@ -1,1 +0,0 @@
-import{W as t,r,X as o}from"./index-BnJknMvC.js";const u=t("msg",()=>{const s=r();return{msg:o({get:()=>s.value,set:e=>{s.value=e}}),_msg:s}},{persist:!0});export{u};
--- assets/useMoney-DMXdsxii.js
+++
@@ -1,1 +0,0 @@
-import{W as t,r as s,X as n}from"./index-BnJknMvC.js";const u=t("money",()=>{const e=s();return{money:n({get:()=>e.value,set:o=>{e.value=o}}),_money:e}},{persist:!0});export{u};
