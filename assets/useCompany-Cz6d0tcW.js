8f65d8e323ce4725ec0eb25d2e55e466877d3d58
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-27 14:04:23 +0800

    Delete assets directory

--- assets/useCompany-Cz6d0tcW.js
+++
@@ -1,1 +0,0 @@
-import{Y as e,r as s,Z as o}from"./index-ConcfWpS.js";const u=e("company",()=>{const t=s();return{data:o({get:()=>t.value,set:a=>{t.value=a}}),_data:t}},{persist:!0});export{u};
--- assets/useMessage-CVdUbcq6.js
+++
@@ -1,1 +0,0 @@
-import{Y as t,r,Z as o}from"./index-ConcfWpS.js";const u=t("msg",()=>{const s=r();return{msg:o({get:()=>s.value,set:e=>{s.value=e}}),_msg:s}},{persist:!0});export{u};
