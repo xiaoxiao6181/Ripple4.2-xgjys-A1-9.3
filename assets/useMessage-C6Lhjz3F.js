67baaab8539f311dc5251359174795a6d9e62da7
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-27 14:05:42 +0800

    Add files via upload

---
+++ assets/useMessage-C6Lhjz3F.js
@@ -0,0 +1,1 @@
+import{Y as t,r,Z as o}from"./index-CiIa-HTw.js";const u=t("msg",()=>{const s=r();return{msg:o({get:()=>s.value,set:e=>{s.value=e}}),_msg:s}},{persist:!0});export{u};
---
+++ assets/useMoney-COrRuUC9.js
@@ -0,0 +1,1 @@
+import{Y as t,r as s,Z as n}from"./index-CiIa-HTw.js";const u=t("money",()=>{const e=s();return{money:n({get:()=>e.value,set:o=>{e.value=o}}),_money:e}},{persist:!0});export{u};
